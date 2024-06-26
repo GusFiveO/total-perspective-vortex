77694e39a55083541577559c5f3c7401aed1b081
Augustin LORAIN <alorain@made-f0Cr4s4.clusters.42paris.fr>
2024-06-26 18:21:22 +0200

    epochs and evoked

--- visualize.py
+++ visualize.py
@@ -9,20 +9,45 @@ try:
 
     raw = mne.io.read_raw_edf(data_file, preload=True)
 
-    raw.compute_psd().plot(
-        picks="data",
-        exclude="bads",
-        amplitude=False,
-        average=True,
-        spatial_colors=False,
+    new_ch_names = [s.replace(".", "") for s in raw.ch_names]
+
+    raw.rename_channels(
+        {original: new for original, new in zip(raw.info["ch_names"], new_ch_names)}
     )
-    raw.plot(duration=5, scalings="auto")
-    print(raw.info)
-    print(raw.ch_names)
-    print(raw.get_data())
     raw.filter(1, 50)
-    print(raw.info)
-    raw.plot(duration=5, scalings="auto")
+
+    events, event_id = mne.events_from_annotations(raw)
+    tmin = -0.2
+    tmax = 0.5
+    epochs = mne.Epochs(
+        raw, events, event_id, tmin, tmax, baseline=(None, 0), preload=True
+    )
+
+    T0_epochs = epochs["T0"]
+    T0_epochs.plot(events=True, scalings="auto")
+    T0_evoked = T0_epochs.average()
+    T0_evoked.plot_image()
+    T0_evoked.plot()
+
+    print(f"epochs {epochs['T0']}")
     plt.show()
 except Exception as e:
     print(e)
+
+    # new_ch_names = [
+    #     s.replace(".", "")
+    #     .replace("c", "C")
+    #     .replace("Cp", "CP")
+    #     .replace("f", "F")
+    #     .replace("t", "T")
+    #     .replace("Tp", "TP")
+    #     .replace("o", "O")
+    #     for s in raw.ch_names
+    # ]
+
+    # layout = mne.channels.read_layout("EEG1005")
+    # print(layout.names)
+    # picks = []
+    # for channel in new_ch_names:
+    #     picks.append(layout.names.index(channel))
+    # display = layout.plot(picks=picks)
