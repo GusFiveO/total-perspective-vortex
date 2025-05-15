06911b942af5930434ae76fa31d1cc8747e6abf2
Augustin LORAIN <augustinlorain@gmail.com>
2025-05-15 18:26:05 +0200

    feat(train.py): preprocess pipeline

--- train.py
+++ train.py
@@ -24,45 +24,58 @@ def parse_args():
     parser.add_argument(
         "--level", type=int, default=4, help="Decomposition level (default: 4)"
     )
+    parser.add_argument(
+        "--tmin", type=float, default=0, help="Epochs starting time"
+    )
+    parser.add_argument(
+        "--tmax", type=float, default=2, help="Epochs ending time"
+    )
     return parser.parse_args()
+
+
+def preprocessing(raw_signal, wavelet, level):
+    pipeline = make_pipeline(
+        BandpassFilter(l_freq=1.0, h_freq=40.0),
+        WaveletDenoiser(wavelet=wavelet, level=level),
+    )
+
+    preprocessed_signal = pipeline.fit_transform(raw_signal)
+    return preprocessed_signal
+
+
+def split_epochs(signal, tmin, tmax):
+    events, event_id = mne.events_from_annotations(
+        signal,
+    )
+
+    epochs = mne.Epochs(
+        raw,
+        events,
+        event_id=event_id,
+        tmin=tmin,
+        tmax=tmax,
+        baseline=None,
+        preload=True,
+        verbose=False,
+    )
+
+    return epochs
+
+
+def training(epochs):
+    print(epochs.event_id)
+    print(epochs.get_data())
 
 
 if __name__ == "__main__":
     args = parse_args()
     raw = mne.io.read_raw_edf(args.edf_file, preload=True, verbose=True)
-    # print(f"Raw data shape: {raw._data.shape}")
-    # events, event_id = mne.events_from_annotations(raw)
-    # print("Data loaded successfully.")
-    # print("Applying bandpass filter...")
-    # raw_filt = filter_bandpass(raw)
-    # print("Bandpass filter applied.")
-    # print("Applying wavelet denoising to all channels...")
-    # eeg_data, times = raw_filt.get_data(return_times=True)
-    # ch_names = raw.ch_names
-    # denoised_signals = denoise_signal(
-    #     eeg_data, wavelet_name=args.wavelet, level=args.level
-    # )
-    # print("Wavelet denoising completed.")
-    pipeline = make_pipeline(
-        BandpassFilter(l_freq=1.0, h_freq=40.0),
-        WaveletDenoiser(wavelet=args.wavelet, level=args.level),
-        CustomCSP(
-            n_components=4,
-        ),
-    )
+    preprocessed_signal = preprocessing(raw, args.wavelet, args.level)
+    print(preprocessed_signal.info)
+    # preprocessed_signal.plot()
+    # plt.show()
 
-    raw_filt = pipeline.fit_transform(raw)
-    events, event_id = mne.events_from_annotations(raw)
-    # plot_signals_with_events(
-    #     raw_filt,
-    #     raw.ch_names,
-    #     raw.times,
-    #     raw.info["sfreq"],
-    #     events,
-    #     event_id,
-    #     title="EEG Signals with Events",
-    #     output_path="output_signals_with_events.png",
-    # )
-    print(raw_filt.info)
-    raw_filt.plot()
-    plt.show()
+    epochs = split_epochs(preprocessed_signal, args.tmin, args.tmax)
+    print(epochs.info)
+
+    training(epochs)
